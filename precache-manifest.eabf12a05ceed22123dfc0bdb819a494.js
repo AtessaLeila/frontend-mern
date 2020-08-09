aeaa563edb0c4a0d0c9ff8722fb8e879ac70d76b
“Atessadai” <Atessa.dailami@gmail.com>
2020-08-09 15:57:41 -0400

    Updates

---
+++ precache-manifest.eabf12a05ceed22123dfc0bdb819a494.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9652bd2ade50add39b95e830af40bd",
+    "url": "/frontend-mern/index.html"
+  },
+  {
+    "revision": "c7579b62648278c2825a",
+    "url": "/frontend-mern/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "72c8f10175ee810ed150",
+    "url": "/frontend-mern/static/css/main.1e79c742.chunk.css"
+  },
+  {
+    "revision": "c7579b62648278c2825a",
+    "url": "/frontend-mern/static/js/2.27e81eac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-mern/static/js/2.27e81eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c8f10175ee810ed150",
+    "url": "/frontend-mern/static/js/main.a16b62b3.chunk.js"
+  },
+  {
+    "revision": "249cb872932071a9cf27",
+    "url": "/frontend-mern/static/js/runtime-main.1bdc216f.js"
+  },
+  {
+    "revision": "2a0f83178cc57091c9be596980601ab0",
+    "url": "/frontend-mern/static/media/background.2a0f8317.jpg"
+  },
+  {
+    "revision": "12332ba7e7a3173aa7910b519ce95ef7",
+    "url": "/frontend-mern/static/media/wsj.12332ba7.png"
+  }
+]);
